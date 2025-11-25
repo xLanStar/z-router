a04ba97a0da2b828d06a7169586e77a2df23ab50
xLanStar <danny95624268@gmail.com>
2025-11-26 06:30:03 +0800

    fix: update button navigation to use location context for canGoBack and canGoForward

--- example/src/app/_main.tsx
+++ example/src/app/_main.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/button";
-import { Outlet, useRouter } from "@modastar/z-router";
+import { Outlet, useLocation, useRouter } from "@modastar/z-router";
 import React from "react";
 
 export const MainLayout: React.FC = () => {
   const router = useRouter();
+  const location = useLocation();
   return (
     <div className="flex h-full w-full flex-col">
       <div className="p-3 bg-gray-200">
@@ -11,11 +12,11 @@ export const MainLayout: React.FC = () => {
       </div>
       <div className="flex-1 p-3 flex flex-col gap-2">
         <div className="flex gap-2">
-          <Button disabled={!router.canGoBack} onClick={() => router.back()}>
+          <Button disabled={!location.canGoBack} onClick={() => router.back()}>
             Back
           </Button>
           <Button
-            disabled={!router.canGoForward}
+            disabled={!location.canGoForward}
             onClick={() => router.forward()}
           >
             Forward
