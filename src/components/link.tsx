1a1ec2cce6ac4a46e0b435e93f7893b2fff89d93
xLanStar <danny95624268@gmail.com>
2025-11-27 05:38:09 +0800

    perf: memorize link component

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,25 +1,19 @@
 import { useRouter } from "@/hooks/useRouter.js";
 import type { NavigateOptions } from "@/types.js";
+import { memo } from "react";
 
 export type LinkProps = React.ComponentPropsWithoutRef<"a"> & NavigateOptions;
 
-export const Link: React.FC<LinkProps> = ({
-  to,
-  replace,
-  transition,
-  duration,
-  onFinish,
-  ...props
-}) => {
+export const Link: React.FC<LinkProps> = memo((props) => {
   const router = useRouter();
   return (
     <a
-      {...props}
-      href={to}
+      href={props.to}
       onClick={(e) => {
         e.preventDefault();
-        router.navigate({ to, replace, transition, duration, onFinish });
+        router.navigate(props);
       }}
+      {...props}
     />
   );
-};
+});
